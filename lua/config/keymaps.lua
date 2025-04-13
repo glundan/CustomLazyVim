8778801e4038451416fa4a351893c10847797f74
gundan <forsendinglogfiles@gmail.com>
2025-04-13 06:40:10 +0000

    Added custom buffer cycling

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,11 @@ vim.keymap.set("n", "<M-Left>", "<C-w>h", { desc = "Go to Left Window", remap = 
 vim.keymap.set("n", "<M-Down>", "<C-w>j", { desc = "Go to Lower Window", remap = true })
 vim.keymap.set("n", "<M-Up>", "<C-w>k", { desc = "Go to Upper Window", remap = true })
 vim.keymap.set("n", "<M-Right>", "<C-w>l", { desc = "Go to Right Window", remap = true })
+
+-- Unbind default next/prev buffer
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+
+-- Remap with custom next/prev buffer
+vim.keymap.set("n", "<S-Left>", "<Cmd>BufferLineCyclePrev<CR>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<S-Right>", "<Cmd>BufferLineCycleNext<CR>", { desc = "Next Buffer" })
